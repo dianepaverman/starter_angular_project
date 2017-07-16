3bec3af9cb27f6cb6bb853238763a710648f42f7
Diane Paverman <dianepaverman@Dianes-iMac.home>
2017-07-16 13:44:51 -0400

    adding files to index

---
+++ client/app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl',
+        controllerAs: 'signup'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
